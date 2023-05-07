2d40944c30d8975cf2aa16655faab62d400d4bf4
robinborth <robin.borth@tum.de>
2023-05-07 21:44:37 +0200

    feat: done

--- lib/utils.py
+++ lib/utils.py
@@ -1,17 +1,17 @@
 import cv2
 from tqdm import tqdm
 from PIL import Image
+import numpy as np
+from typing import Generator
 
 
 def frame_generator(
     video_path: str,
     sample_rate: int = 25,
     max_frames: int | None = None,
-):
+) -> Generator[np.ndarray, None, None]:
     cap = cv2.VideoCapture(video_path)
     total_frames = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
-
-    # algorithm
     success = cap.grab()  # get the next frame
     fno = 0
     with tqdm(total=total_frames) as pbar:
@@ -27,17 +27,35 @@ def frame_generator(
                 break
 
 
-def slice_frame(
-    frame,
+def get_random_frame(video_path: str) -> Image:
+    return list(
+        frame_generator(
+            video_path=video_path,
+            sample_rate=1000,
+            max_frames=2,
+        )
+    )[-1]
+
+
+def slice_frame_generator(
+    frame: np.ndarray,
     *,
-    left: int = 35,
-    right: int = 1305,
-    top: int = 250,
-    bottom: int = 1890,
+    pleft: int = 0,
+    pright: int = 0,
+    ptop: int = 0,
+    pbottom: int = 0,
 ):
-    return frame[top:bottom, left:right, :]
+    height, width, _ = frame.shape
+    return frame[ptop : height - pbottom, pleft : width - pright, :]
 
 
-def show_frame(frame):
-    img = Image.fromarray(frame, "RGB")
-    return img
+def get_image(frame):
+    return Image.fromarray(frame, "RGB")
+
+
+def save_frames(slides: list[np.ndarray], path: str = "out.pdf") -> None:
+    images = []
+    for slide in slides:
+        img = get_image(slide)
+        images.append(img)
+    images[0].save(path, save_all=True, append_images=images[1:])
